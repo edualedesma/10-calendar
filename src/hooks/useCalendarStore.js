6a2e73538adf8b0c4234cc3afcc73ed8d7443999
Eduardo Acuña <eaculed@gmail.com>
2023-10-27 10:36:53 +0100

    Add new events to database

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -1,25 +1,26 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { onAddNewEvent, onDeleteEvent, onSetActiveEvent, onUpdateEvent } from '../store/calendar/calendarSlice';
+import { calendarApi } from '../api';
 
 export const useCalendarStore = () => {
 	const dispatch = useDispatch();
 
 	const { events, activeEvent } = useSelector( state => state.calendar );
+	const { user } = useSelector( state => state.auth );
 
 	const setActiveEvent = ( calendarEvent ) => {
 		dispatch( onSetActiveEvent( calendarEvent ) );
 	}
 
 	const startSavingEvent = async( calendarEvent ) => {
-		// TODO: go to backend
-
-		// TODO: Everything okey
+		// TODO: Update event
 		if ( calendarEvent._id ) {
 			// Updating
 			dispatch( onUpdateEvent({ ...calendarEvent }) );
 		} else {
 			// Adding
-			dispatch( onAddNewEvent({ ...calendarEvent, _id: new Date().getTime() }) );
+			const { data } = await calendarApi.post('/events', calendarEvent);
+			dispatch( onAddNewEvent({ ...calendarEvent, id: data.event.id, user }) );
 		}
 
 	}
